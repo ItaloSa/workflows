abeb348b57470c1c32258d96f11320e7f30bf879
Italo Sousa <italo.sousa@ifood.com.br>
2022-03-15 10:25:28 -0300

    ci: Publish

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+
+const sum = (a: number, b: number) => console.log(a + b);
+
+const min = (a: number, b: number) => console.log(a - b);
+
+export default {
+  sum,
+  min,
+};
